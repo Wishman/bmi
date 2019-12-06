01337440ed167b9e46b1205e473910a9ff17fb60
Wishman <gjwisher@aol.com>
2019-12-06 13:43:49 +0100

    stateless

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -13,17 +12,71 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: <Widget>[
+          // first row - two containers
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    colour: Color(0xFF1D1E33),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: Color(0xFF1D1E33),
+                  ),
+                )
+              ],
+            ),
+          ),
+
+          // second "row" - 1 container - therefore no Row()
+          Expanded(
+            child: ReusableCard(
+              colour: Color(0xFF1D1E33),
+            ),
+          ),
+
+          // third row - two containers
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    colour: Color(0xFF1D1E33),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: Color(0xFF1D1E33),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
-      /*
-      floatingActionButton: Theme(
-        // override overall theme on only this occasion
-        data: ThemeData(accentColor: Colors.purple),
-        child: FloatingActionButton(
-          child: Icon(Icons.add),
-        ),
-      ),*/
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  // prop for changing color later
+  final Color colour;
+
+  // constr - make mandatory with @required annotation
+  ReusableCard({@required this.colour});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
     );
   }
 }
